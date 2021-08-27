d18b7ed19122c2ae4c93829ce51583b6a890e0fb
Alan-Kuan <kuanhaoan@gmail.com>
2021-08-27 21:57:39 +0800

    Add lazygit float term with toggleterm.

--- lua/configs/toggleterm.lua
+++ lua/configs/toggleterm.lua
@@ -1,4 +1,5 @@
 local map = vim.api.nvim_set_keymap
+local Terminal = require('toggleterm.terminal').Terminal
 
 require('toggleterm').setup {
     size = function (term)
@@ -19,3 +20,19 @@ local noremap = { noremap = true }
 -- this is actually <C-/>; however, vim recognized it as <C-_>
 map('n', '<C-_>', ':ToggleTerm direction=horizontal<CR>', noremap)
 map('n', '<C-\\>', ':ToggleTerm direction=vertical<CR>', noremap)
+
+local lazygit = Terminal:new {
+    cmd = 'lazygit',
+    dir = 'git_dir',
+    direction = 'float',
+    hidden = true,
+    float_opts = {
+        border = 'curved',
+        height = 30
+    }
+}
+function LazygitToggle()
+    lazygit:toggle()
+end
+
+map('n', '<leader>g', ':lua LazygitToggle()<CR>', { noremap = true, silent = true })
