325f8addd125ed0a4fa9893b3ad2089c9aa7f938
Alan-Kuan <kuanhaoan@gmail.com>
2021-09-22 22:27:33 +0800

    Remove a comment.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -26,7 +26,6 @@ require('packer').startup(function (use)
             require('configs.lualine')
         end
     }
-    -- tabnew problem
     use {
         'kyazdani42/nvim-tree.lua',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true },
