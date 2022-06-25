179043a78dfddd50a425b97455a16301da7a786a
Alan-Kuan <kuanhaoan@gmail.com>
2022-06-25 12:21:18 +0800

    style: update dashboard

--- lua/configs/dashboard.lua
+++ lua/configs/dashboard.lua
@@ -1,30 +1,40 @@
-local g = vim.g
+local db = require('dashboard')
 local map = vim.api.nvim_set_keymap
 
-g.dashboard_default_executive = 'telescope'
-
-g.dashboard_custom_shortcut = {
-    book_marks         = '<leader>fb',
-    change_colorscheme = '<leader>tc',
-    find_file          = '<leader>ff',
-    find_history       = '<leader>fH',
-    find_word          = '<leader>fg',
-    last_session       = '<leader>sl',
-    new_file           = '<leader>cn'
-}
-
-local silent = { silent = true }
-map('n', '<leader>ss', ':<C-u>SessionSave<CR>', silent)
-map('n', '<leader>sl', ':<C-u>SessionLoad<CR>', silent)
-map('n', '<leader>cn', ':enew<CR>', silent)
-
-g.dashboard_custom_header = {
+db.custom_header = {
+    '', '',
     [[   _  _                              _            ]],
     [[  | \| |    ___     ___    __ __    (_)    _ __   ]],
     [[  | .` |   / -_)   / _ \   \ V /    | |   | '  \  ]],
     [[  |_|\_|   \___|   \___/   _\_/_   _|_|_  |_|_|_| ]],
     [[ _|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""| ]],
-    [[ "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-' ]]
+    [[ "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-' ]],
+    '', '',
 }
 
-g.dashboard_custom_footer = { ' Enjoy your day.' }
+db.custom_center = {
+    { icon = '  ', desc = 'New File                ', shortcut = '<leader>cn', action = 'DashboardNewFile' },
+    { icon = '  ', desc = 'Book marks              ', shortcut = '<leader>fb', action = 'Telescope marks' },
+    { icon = '  ', desc = 'Change Colorscheme      ', shortcut = '<leader>cc', action = 'Telescope colorscheme' },
+    { icon = '  ', desc = 'Find File               ', shortcut = '<leader>ff', action = 'Telescope find_files' },
+    { icon = '  ', desc = 'Find History            ', shortcut = '<leader>fh', action = 'Telescope oldfiles' },
+    { icon = '  ', desc = 'Find Word               ', shortcut = '<leader>fg', action = 'Telescope live_grep' },
+    { icon = '  ', desc = 'Last Session            ', shortcut = '<leader>sl', action = 'SessionLoad' },
+    { icon = '  ', desc = 'Notifications           ', shortcut = '<leader>nc', action = 'Telescope notify' },
+}
+
+db.custom_footer = {
+    '', '',
+    ' Enjoy your day.'
+}
+
+local silent = { silent = true }
+map('n', '<leader>ss', ':SessionSave<CR>', silent)
+map('n', '<leader>sl', ':SessionLoad<CR>', silent)
+map('n', '<leader>cn', ':DashboardNewFile<CR>', silent)
+
+vim.cmd('hi DashboardHeader     guifg=#42ff7b')
+vim.cmd('hi DashboardCenterIcon guifg=white')
+vim.cmd('hi DashboardCenter     guifg=#42a1ff')
+vim.cmd('hi DashboardShortCut   guifg=skyblue')
+vim.cmd('hi DashboardFooter     guifg=#ffd062')
--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,10 +1,11 @@
 local map = vim.api.nvim_set_keymap
 local noremap = { noremap = true }
 
+require('telescope').load_extension('notify')
+
 map('n', '<leader>ff', ":lua require('telescope.builtin').find_files()<CR>", noremap)
 map('n', '<leader>fg', ":lua require('telescope.builtin').live_grep()<CR>", noremap)
 map('n', '<leader>fb', ":lua require('telescope.builtin').marks()<CR>", noremap)
-map('n', '<leader>fB', ":lua require('telescope.builtin').buffers()<CR>", noremap)
-map('n', '<leader>fh', ":lua require('telescope.builtin').help_tags()<CR>", noremap)
-map('n', '<leader>fH', ":lua require('telescope.builtin').oldfiles()<CR>", noremap)
-map('n', '<leader>tc', ":lua require('telescope.builtin').colorscheme()<CR>", noremap)
+map('n', '<leader>fh', ":lua require('telescope.builtin').oldfiles()<CR>", noremap)
+map('n', '<leader>cc', ":lua require('telescope.builtin').colorscheme()<CR>", noremap)
+map('n', '<leader>nc', ":lua require('telescope').extensions.notify.notify()<CR>", noremap)
