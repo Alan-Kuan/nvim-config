392d050a2cc16b6ecd24369dc54c1fd8e411c1d4
Alan-Kuan <kuanhaoan@gmail.com>
2023-02-13 23:48:15 +0800

    feat: add headlines.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -214,8 +214,49 @@ require('lazy').setup({
         'plasticboy/vim-markdown',
         dependencies = 'godlygeek/tabular',
         ft = 'markdown',
+        event = 'VeryLazy',
         config = function ()
             require('configs.markdown')
         end
-    }
+    },
+    {
+        'lukas-reineke/headlines.nvim',
+        ft = 'markdown',
+        event = 'VeryLazy',
+        config = function ()
+            require('headlines').setup {
+                markdown = {
+                    query = vim.treesitter.parse_query(
+                        'markdown',
+                        [[
+                            (atx_heading [
+                                (atx_h1_marker)
+                                (atx_h2_marker)
+                                (atx_h3_marker)
+                                (atx_h4_marker)
+                                (atx_h5_marker)
+                                (atx_h6_marker)
+                            ] @headline)
+
+                            (thematic_break) @dash
+
+                            (fenced_code_block) @codeblock
+
+                            (block_quote_marker) @quote
+                            (block_quote (paragraph (inline (block_continuation) @quote)))
+                        ]]
+                    ),
+                    headline_highlights = { 'Headline' },
+                    codeblock_highlight = 'CodeBlock',
+                    dash_highlight = 'Dash',
+                    dash_string = '-',
+                    quote_highlight = 'Quote',
+                    quote_string = '┃',
+                    fat_headlines = true,
+                    fat_headline_upper_string = '▃',
+                    fat_headline_lower_string = '🬂',
+                },
+            }
+        end
+    },
 })
