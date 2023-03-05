dcc3ea322035b89ccc3726d2a8a53be9555e75b3
Alan-Kuan <kuanhaoan@gmail.com>
2023-03-05 18:40:56 +0800

    style: add italic style to some markdown related highlight groups

--- lua/highlightings.lua
+++ lua/highlightings.lua
@@ -4,10 +4,16 @@ local function update_highlight(name, hl)
     vim.api.nvim_set_hl(0, name, new_hl)
 end
 
--- Word Highlighting
+-- Word
 vim.api.nvim_set_hl(0, 'Visual', { bg = '#31474d' })
 vim.api.nvim_set_hl(0, 'Search', { bg = '#314d34' })
 vim.api.nvim_set_hl(0, 'IncSearch', { bg = '#314d34' })
 
--- Comment Highlighting
+-- Comment
 update_highlight('Comment', { italic = true })
+
+-- Markdown
+update_highlight('htmlItalic', { italic = true })
+update_highlight('htmlBoldItalic', { italic = true })
+update_highlight('htmlUnderlineItalic', { italic = true })
+update_highlight('htmlBoldUnderlineItalic', { italic = true })
