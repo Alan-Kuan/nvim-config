3bb58e15be67049d5aff95fa5ca1905e3148063d
Alan-Kuan <kuanhaoan@gmail.com>
2023-03-10 15:33:35 +0800

    style: update search highlight

--- lua/highlightings.lua
+++ lua/highlightings.lua
@@ -6,8 +6,8 @@ end
 
 -- Word
 vim.api.nvim_set_hl(0, 'Visual', { bg = '#31474d' })
-vim.api.nvim_set_hl(0, 'Search', { bg = '#314d34' })
-vim.api.nvim_set_hl(0, 'IncSearch', { bg = '#314d34' })
+vim.api.nvim_set_hl(0, 'Search', { bg = '#5f3c25' })
+vim.api.nvim_set_hl(0, 'IncSearch', { bg = '#5f3c25' })
 
 -- Comment
 update_highlight('Comment', { italic = true })
