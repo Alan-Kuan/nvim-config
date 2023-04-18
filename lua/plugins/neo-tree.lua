ce4ef23b0baedb3ddbf2679c0a3238e5b9692790
Alan-Kuan <kuanhaoan@gmail.com>
2023-04-18 22:08:16 +0800

    style: update highlightings for neo-tree git source

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -119,8 +119,13 @@ return {
         config = function (_, opts)
             require('neo-tree').setup(opts)
 
-            vim.api.nvim_set_hl(0, 'NeoTreeGitConflict', { bold = true, fg = '#ff8700' })
-            vim.api.nvim_set_hl(0, 'NeoTreeGitUntracked', { fg = '#ff8700' })
+            vim.api.nvim_set_hl(0, 'NeoTreeGitAdded', { link = 'GruvboxGreen' })
+            vim.api.nvim_set_hl(0, 'NeoTreeGitDeleted', { link = 'GruvboxRed' })
+            vim.api.nvim_set_hl(0, 'NeoTreeGitModified', { link = 'GruvboxOrange' })
+            vim.api.nvim_set_hl(0, 'NeoTreeGitConflict', { link = 'GruvboxOrange' })
+            vim.api.nvim_set_hl(0, 'NeoTreeGitStaged', { link = 'GruvboxBlue' })
+            vim.api.nvim_set_hl(0, 'NeoTreeGitUnstaged', { link = 'GruvboxBlue' })
+            vim.api.nvim_set_hl(0, 'NeoTreeGitUntracked', { link = 'GruvboxYellow' })
         end
     }
 }
