f38a92efd1c36f72bdd25911d1f5c534822b9d8e
Alan-Kuan <kuanhaoan@gmail.com>
2023-08-20 12:18:48 +0800

    chore: revert version of nvim-window-picker

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -213,25 +213,19 @@ return {
       'MunifTanjim/nui.nvim',
       {
         's1n7ax/nvim-window-picker',
-        version = '2.*',
+        version = '1.*',
         event = 'VeryLazy',
         opts = {
+          autoselect_one = true,
+          include_current = false,
           filter_rules = {
-            autoselect_one = true,
-            include_current_win = false,
             -- ignore windows by buffer options
             bo = {
               filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
               buftype = { 'terminal', 'quickfix' },
             },
           },
-          highlights = {
-            statusline = {
-              unfocused = {
-                bg = '#a95721',
-              }
-            },
-          },
+          other_win_hl_color = '#a95721',
         },
       },
     },
