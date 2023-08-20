40e7d67f183f53ccfdbfb44122d2003662c01d28
Alan-Kuan <kuanhaoan@gmail.com>
2023-08-20 12:15:23 +0800

    chore: update plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -370,7 +370,7 @@ return {
   },
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
+    branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
       'rcarriga/nvim-notify',
