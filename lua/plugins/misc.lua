6951eb896526b9bffc0ae79d215600872fe162d0
Alan-Kuan <kuanhaoan@gmail.com>
2023-10-13 22:05:02 +0800

    feat: specify the pdf viewer used in typst.vim

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -40,6 +40,9 @@ return {
     'kaarmu/typst.vim',
     ft = 'typst',
     lazy = false,
+    init = function ()
+      vim.g.typst_pdf_viewer = 'zathura'
+    end
   },
   {
     'Fymyte/rasi.vim',
