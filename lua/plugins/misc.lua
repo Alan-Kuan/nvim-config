c9b3dc2ed5cb1c4859befa260fbfcb56868dfbeb
Alan-Kuan <kuanhaoan@gmail.com>
2024-03-11 00:06:08 +0800

    feat: add substitute.nvim

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -9,6 +9,21 @@ return {
     end,
   },
   {
+    'gbprod/substitute.nvim',
+    event = 'VeryLazy',
+    opts = {
+      highlight_substituted_text = {
+        enabled = false,
+      },
+    },
+    config = function (_, opts)
+      require('substitute').setup(opts)
+
+      vim.keymap.set('n', '<C-p>', require('substitute').operator, { desc = 'Substitute operator' })
+      vim.keymap.set('x', '<C-p>', require('substitute').visual, { desc = 'Substitute selection without overwriting the buffer' })
+    end
+  },
+  {
     'mrjones2014/smart-splits.nvim',
     opts = {
       default_amount = 1,
