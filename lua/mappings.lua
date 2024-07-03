ee5df79516679e736fd58a8a23f1170c3ef0ded0
Alan-Kuan <kuanhaoan@gmail.com>
2024-07-04 00:01:41 +0800

    feat!: remove cut to system related key bindings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -19,15 +19,7 @@ nnoremap('<C-n>', function()
 -- select all
 nnoremap('<leader>a', 'ggVG', 'Select all')
 -- copy to the system clipboard
-nnoremap('<leader>yy', '"+yy', 'Copy to the system clipboard')
 vnoremap('<leader>y', '"+y', 'Copy to the system clipboard')
--- cut to the system clipboard
-nnoremap('<leader>x', '"+x', 'Cut to the system clipboard')
-vnoremap('<leader>x', '"+x', 'Cut to the system clipboard')
-nnoremap('<leader>dd', '"+dd', 'Cut to the system clipboard')
-vnoremap('<leader>d', '"+d', 'Cut to the system clipboard')
-nnoremap('<leader>D', '"+D', 'Cut to the system clipboard')
-vnoremap('<leader>D', '"+D', 'Cut to the system clipboard')
 -- paste from the system clipboard
 nnoremap('<leader>p', '"+p', 'Paste from the system clipboard')
 vnoremap('<leader>p', '"+p', 'Paste from the system clipboard')
