03d4979ebbfc572241e19d4cff976b771e05d799
Alan-Kuan <kuanhaoan@gmail.com>
2024-08-20 11:14:52 +0800

    vimtex: hide quickfix by default

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -2,6 +2,9 @@ return {
   {
     'lervag/vimtex',
     lazy = false,
-    init = function() vim.g.vimtex_view_method = 'zathura' end,
+    init = function()
+      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_quickfix_mode = 0
+    end,
   },
 }
