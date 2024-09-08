11cf8651e08000266cb8870dfb77b727d1c7bd6b
Alan-Kuan <kuanhaoan@gmail.com>
2024-09-08 17:38:50 +0800

    bibtex: add --shell-escape compiler option

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -5,6 +5,15 @@ return {
     init = function()
       vim.g.vimtex_view_method = 'zathura'
       vim.g.vimtex_quickfix_mode = 0
+      vim.g.vimtex_compiler_latexmk = {
+        options = {
+          '-verbose',
+          '-file-line-error',
+          '-synctex=1',
+          '-interaction=nonstopmode',
+          '-shell-escape',
+        }
+      }
     end,
   },
 }
