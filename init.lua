af6020c4fdd7f118283ded359948304026615c17
Alan-Kuan <kuanhaoan@gmail.com>
2025-01-05 16:05:25 +0800

    lazy: update init.lua

--- init.lua
+++ init.lua
@@ -2,16 +2,27 @@ require('options')
 require('mappings')
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system {
     'git',
     'clone',
     '--filter=blob:none',
     'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+    '--branch=stable',
     lazypath,
   }
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins')
+require('lazy').setup({
+  spec = {
+    { import = 'plugins' },
+  },
+  install = {
+    colorscheme = {
+      'ayu',
+      'everforest',
+    },
+  },
+  checker = { enabled = true },
+})
