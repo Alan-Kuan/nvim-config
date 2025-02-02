19dcdd10947c9ad2c40c765a117e5db47c9172c7
Alan-Kuan <kuanhaoan@gmail.com>
2025-02-02 21:56:42 +0800

    feat!: remove rasi.vim

--- lua/plugins/ft/misc.lua
+++ lua/plugins/ft/misc.lua
@@ -6,11 +6,6 @@ return {
     init = function() vim.g.typst_pdf_viewer = 'zathura' end,
   },
   {
-    'Fymyte/rasi.vim', -- NOTE: rasi is rofi's config file type
-    requires = 'nvim-treesitter/nvim-treesitter',
-    ft = 'rasi',
-  },
-  {
     'davidmh/mdx.nvim',
     dependencies = 'nvim-treesitter/nvim-treesitter',
     opts = {},
