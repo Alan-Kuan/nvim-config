4b69bed18c8d6c7e45c825013bccde1f269f2722
Alan-Kuan <kuanhaoan@gmail.com>
2025-02-22 15:49:21 +0800

    latex: disable vimtex syntax highlighting

--- lua/plugins/ft/latex.lua
+++ lua/plugins/ft/latex.lua
@@ -4,6 +4,7 @@ return {
     lazy = false,
     init = function()
       vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_syntax_enabled = 0
       vim.g.vimtex_quickfix_mode = 0
       vim.g.vimtex_compiler_latexmk = {
         options = {
