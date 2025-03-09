969eb35eae502d5c0974704ed8d68abcd51be566
Alan-Kuan <kuanhaoan@gmail.com>
2025-03-09 13:09:17 +0800

    fix: should only add TypstWatch binding in typst file

--- lua/plugins/ft/misc.lua
+++ lua/plugins/ft/misc.lua
@@ -3,7 +3,7 @@ return {
     'kaarmu/typst.vim',
     ft = 'typst',
     keys = {
-      { '<LocalLeader>t', '<Cmd>TypstWatch<CR>', desc = 'Start Typst Preview' },
+      { '<LocalLeader>t', '<Cmd>TypstWatch<CR>', desc = 'Start Typst Preview', ft = 'typst' },
     },
     config = function() vim.g.typst_pdf_viewer = 'zathura' end,
   },
