914188ecd18e46e212da1bdebd1465e812984d2a
Alan-Kuan <kuanhaoan@gmail.com>
2025-03-09 10:50:15 +0800

    toggleterm: lazy load toggleterm.nvim by keys & change the key binding to save & run the code

--- lua/plugins/ui/terminal.lua
+++ lua/plugins/ui/terminal.lua
@@ -1,6 +1,45 @@
 return {
   {
     'akinsho/toggleterm.nvim',
+    keys = function()
+      local keys = { '<C-`>' }
+
+      -- Key bindings for executing the codes in current file
+      local cmds = {
+        ['javascript'] = 'node "%:p:."',
+        ['typescript'] = 'deno run "%:p:."',
+        ['python'] = 'python3 "%:p:."',
+        ['c'] = 'gcc "%:p:." -o "%:p:.:r" && "./%:p:.:r"',
+        ['cpp'] = 'g++ "%:p:." -o "%:p:.:r" && "./%:p:.:r"',
+        ['go'] = 'go run "%:p:."',
+      }
+      for ft, cmd in pairs(cmds) do
+        table.insert(keys, {
+          '<LocalLeader>r',
+          ":w <bar> :TermExec cmd='" .. cmd .. "'<CR>",
+          desc = 'Save & Run the code',
+          ft = ft,
+        })
+      end
+
+      -- Key bindings for sending selected lines to the opened terminal
+      local files = {
+        'python',
+        'javascript',
+        'r',
+      }
+      for _, ft in ipairs(files) do
+        table.insert(keys, {
+          '<C-Enter>',
+          ':<C-u>ToggleTermSendVisualLines<CR>',
+          mode = 'x',
+          desc = 'Send selection to the terminal',
+          ft = ft,
+        })
+      end
+
+      return keys
+    end,
     opts = {
       size = function(term)
         if term.direction == 'horizontal' then
@@ -15,63 +54,5 @@ return {
         border = 'curved',
       },
     },
-    config = function(_, opts)
-      require('toggleterm').setup(opts)
-
-      -- Key bindings for executing the codes in current file
-
-      local set_keymap = function(ft, cmd)
-        vim.api.nvim_create_autocmd('Filetype', {
-          group = 'Toggleterm',
-          pattern = { ft },
-          callback = function()
-            vim.keymap.set(
-              'n',
-              '<F5>',
-              ":w <bar> :TermExec cmd='" .. cmd .. "'<CR>",
-              { desc = 'Run the code' }
-            )
-          end,
-        })
-      end
-
-      local cmds = {
-        ['javascript'] = 'node "%:p:."',
-        ['typescript'] = 'deno run "%:p:."',
-        ['python'] = 'python3 "%:p:."',
-        ['c'] = 'gcc "%:p:." -o "%:p:.:r" && "./%:p:.:r"',
-        ['cpp'] = 'g++ "%:p:." -o "%:p:.:r" && "./%:p:.:r"',
-        ['go'] = 'go run "%:p:."',
-      }
-
-      vim.api.nvim_create_augroup('Toggleterm', { clear = true })
-
-      for ft, cmd in pairs(cmds) do
-        set_keymap(ft, cmd)
-      end
-
-      -- Key bindings for sending selected lines to the opened terminal
-
-      local files = {
-        'python',
-        'javascript',
-        'r',
-      }
-
-      for _, ft in ipairs(files) do
-        vim.api.nvim_create_autocmd('Filetype', {
-          group = 'Toggleterm',
-          pattern = { ft },
-          callback = function()
-            vim.keymap.set(
-              'x',
-              '<C-Enter>',
-              ':<C-u>ToggleTermSendVisualLines<CR>',
-              { silent = true, desc = 'Send selection to the terminal' }
-            )
-          end,
-        })
-      end
-    end,
   },
 }
