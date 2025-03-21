ae82a0c96bdb4857e52a29ecec2ee6eb1ca84478
Alan-Kuan <kuanhaoan@gmail.com>
2025-03-22 01:23:03 +0800

    scrollview: hide it on cursor intersect & hide low priority diagnostic symbols

--- lua/plugins/ui/scrollview.lua
+++ lua/plugins/ui/scrollview.lua
@@ -7,11 +7,15 @@ return {
     },
     opts = {
       winblend = 25,
-      signs_column = 1,
+      hide_on_cursor_intersect = true,
       signs_max_per_row = 1,
       cursor_symbol = '•',
       diagnostics_error_symbol = '-',
       diagnostics_warn_symbol = '-',
+      diagnostics_severities = {
+        vim.diagnostic.severity.ERROR,
+        vim.diagnostic.severity.WARN,
+      },
       signs_on_startup = {
         'conflicts',
         'cursor',
