a8d97e43517f18634bddc901578d0a233fdb86cc
Alan-Kuan <kuanhaoan@gmail.com>
2025-12-26 01:09:31 +0800

    treesitter: migrated to latest version and will install ensured treesitters on demand

--- lua/plugins/coding/treesitter.lua
+++ lua/plugins/coding/treesitter.lua
@@ -5,30 +5,80 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     event = { 'BufReadPre', 'BufNewFile', 'BufWritePost' },
-    opts = function ()
-      local parsers = require("nvim-treesitter.parsers")
+    -- Ref: https://github.com/ThorstenRhau/neovim/blob/9918700a29ace6c5caab28ff9266225f88b24bd2/lua/optional/treesitter.lua
+    config = function ()
+      local ts = require('nvim-treesitter')
 
-      local opts = {
-        ensure_installed = {},
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
-      }
+      local pending_bufs = {}
 
-      for lang, enabled in pairs(settings.langs) do
-        -- if a language is enabled and its treesitter exists
-        if enabled and parsers.get_parser_configs()[lang] then
-          table.insert(opts.ensure_installed, lang)
+      -- a helper to start treesitter with retry for async parser installation
+      local function start_with_retry(buf, lang, attempts)
+        attempts = attempts or 10
+        local key = buf .. ':' .. lang
+
+        if pcall(vim.treesitter.start, buf, lang) then
+          pending_bufs[key] = nil
+        elseif attempts > 0 then
+          vim.defer_fn(function()
+            start_with_retry(buf, lang, attempts - 1)
+          end, 500)
+        else
+          -- give up
+          pending_bufs[key] = nil
         end
       end
 
-      return opts
+      local function enable_highlighting(buf, lang)
+        local key = buf .. ':' .. lang
+
+        if not vim.api.nvim_buf_is_valid(buf) then
+          pending_bufs[key] = nil
+          return
+        end
+
+        if pending_bufs[key] then
+          return
+        end
+        pending_bufs[key] = true
+
+        start_with_retry(buf, lang)
+      end
+
+      local group = vim.api.nvim_create_augroup('TreesitterSetup', { clear = true })
+
+      vim.api.nvim_create_autocmd('BufDelete', {
+        group = group,
+        desc = 'Cancel pending treesitter setup',
+        callback = function (event)
+          for key in pairs(pending_bufs) do
+            if key:match('^' .. event.buf .. ':') then
+              pending_bufs[key] = nil
+            end
+          end
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = group,
+        desc = 'Enable treesitter highlighting and indentation',
+        callback = function (event)
+          local lang = vim.treesitter.language.get_lang(event.match) or event.match
+
+          if not settings.langs[lang] then
+            return
+          end
+
+          -- install the treesitter on demand
+          if settings.langs[lang].enabled and not vim.list_contains(ts.get_installed(), lang) then
+            -- NOTE: this is an async function
+            ts.install({ lang })
+          end
+
+          enable_highlighting(event.buf, lang)
+          vim.bo[event.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      })
     end,
-    config = function(_, opts) require('nvim-treesitter.configs').setup(opts) end,
   },
   {
     'nvim-treesitter/nvim-treesitter-context',
